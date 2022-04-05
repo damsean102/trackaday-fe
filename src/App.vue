890073e1c15e8f2eb0b0fe2484b31f8fe0bc091b
Sam Dean <sam.dean@substrakt.com>
2022-04-05 21:45:20 +0100

    UPDATE: View app to display the indexed items

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,62 @@
-<script>
-  import algoliasearch from 'algoliasearch/lite';
-
-  export default {
-    data() {
-      return {
-        searchClient: algoliasearch(
-          'Y9J24EDN41',
-          'f88fc1c54d13cf087ff21fac9294116f'
-        ),
-      };
-    },
-  };
-</script>
-
-
 <template>
     <ais-instant-search index-name="tracks" :search-client="searchClient">
-        <ais-search-box />
+
+        <!-- Search Box -->
+        <ais-search-box show-loading-indicator />
+
+        <!-- Search Results -->
+        <ais-hits>
+
+             <template v-slot="{ items }">
+             <h2>Recently added:</h2>
+                <section v-for="item in items" :key="item.objectID">
+                    <h3>{{ item.name }} - <span v-for="artist in item.artists"> {{ artist }}, </span></h3>
+
+                    <img :src="item.thumbnailURL" :width="150" :height="150" />
+
+                    <audio controls>
+                        <source :src="item.previewUrl">
+                        Your browser does not support the audio element.
+                    </audio>
+
+                    <ul>
+                        <li><a :href="item.externalURL">Track URL</a></li>
+                        <li>Date Added: {{ item.addedAt }}</li>
+                        <li>Duration: {{ item.duration }}</li>
+                        <li><a :href="item.playlistUrl">{{ item.playlistName }}</a></li>
+                    </ul>
+
+                </section>
+            </template>
+        </ais-hits>
+
+       <ais-pagination />
+
     </ais-instant-search>
 </template>
+
+<script>
+    import algoliasearch from 'algoliasearch/lite';
+
+    export default {
+        data() {
+            return {
+                searchClient: algoliasearch(
+                    'Y9J24EDN41',
+                    'f88fc1c54d13cf087ff21fac9294116f'
+                ),
+            };
+        },
+    };
+</script>
 
 <style>
 @import './assets/base.css';
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
 }
 </style>
