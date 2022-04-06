ce1d3accda09d54a3aca761528801bb9e7d249b8
Sam Dean <sam.dean@substrakt.com>
2022-04-06 22:54:42 +0100

    ADD: Tailwind and basic styles

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,44 @@
     <ais-instant-search index-name="tracks" :search-client="searchClient">
 
         <!-- Search Box -->
-        <ais-search-box show-loading-indicator />
+        <ais-search-box show-loading-indicator :class-names="{
+            'ais-SearchBox-form': 'w-full shadow-md',
+        }" />
 
         <!-- Search Results -->
         <ais-hits>
 
              <template v-slot="{ items }">
-             <h2>Recently added:</h2>
-                <section v-for="item in items" :key="item.objectID">
-                    <h3>{{ item.name }} - <span v-for="artist in item.artists"> {{ artist }}, </span></h3>
+                <h2>Recently added:</h2>
+                <div class="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-6 my-12">
 
-                    <img :src="item.thumbnailURL" :width="150" :height="150" />
+                    <section class="border-solid border-2 border-slate-100 p-3" v-for="item in items" :key="item.objectID">
+                        <img :src="item.thumbnailURL" />
 
-                    <audio controls>
-                        <source :src="item.previewUrl">
-                        Your browser does not support the audio element.
-                    </audio>
+                        <h3>{{ item.name }} - <span v-for="artist in item.artists"> {{ artist }}, </span></h3>
 
-                    <ul>
-                        <li><a :href="item.externalURL">Track URL</a></li>
-                        <li>Date Added: {{ item.addedAt }}</li>
-                        <li>Duration: {{ item.duration }}</li>
-                        <li><a :href="item.playlistUrl">{{ item.playlistName }}</a></li>
-                    </ul>
+                        <audio controls>
+                            <source :src="item.previewUrl">
+                            Your browser does not support the audio element.
+                        </audio>
 
-                </section>
+                        <ul>
+                            <li><a :href="item.externalURL">Track URL</a></li>
+                            <li>Date Added: {{ item.addedAt }}</li>
+                            <li>Duration: {{ item.duration }}</li>
+                            <li><a :href="item.playlistUrl">{{ item.playlistName }}</a></li>
+                        </ul>
+                    </section>
+
+                </div>
             </template>
         </ais-hits>
 
-       <ais-pagination />
+        <ais-pagination :class-names="{
+            'ais-Pagination': 'w-full m-12',
+            'ais-Pagination-list': 'flex justify-center',
+            'ais-Pagination-item': 'w-14',
+        }" />
 
     </ais-instant-search>
 </template>
