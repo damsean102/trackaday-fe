2cd19faea05c3e2a10c0c69194abbb797d4b425e
Sam Dean <sam.dean@substrakt.com>
2022-04-15 12:16:46 +0100

    UPDATE: Styling

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,49 @@
 <template>
+
+    <h1 class="text-4xl text-center">Trackaday Archive</h1>
+
     <ais-instant-search index-name="tracks" :search-client="searchClient">
 
         <!-- Search Box -->
         <ais-search-box show-loading-indicator :class-names="{
-            'ais-SearchBox-form': 'w-full shadow-md',
+            'ais-SearchBox': 'my-8 py-4',
+            'ais-SearchBox-form': 'w-full',
+            'ais-SearchBox-submit': 'p-4 hidden',
+            'ais-SearchBox-reset': 'p-4 hidden',
+
         }" />
 
         <!-- Search Results -->
         <ais-hits>
              <template v-slot="{ items }">
-                <h2>Recently added:</h2>
+                <h2 class="text-2xl">Most Recent:</h2>
                <!-- component -->
-                <main class="grid place-items-center min-h-screen min-h-screen p-5">
+                <main class="grid place-items-center py-5">
                     <div>
-                        <section class="grid grid-cols-1 sm:grid-cols-3 gap-3">
+                        <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
 
                             <!-- CARD -->
                             <div v-for="item in items" :key="item.objectID" class="bg-gray-900 shadow-lg rounded p-3">
                                 <div class="group relative">
-                                    <img :src="item.thumbnailURL" class="w-full block rounded hover:cursor-pointer" @click="playSound(item.previewUrl)" />
+                                    <a :href="item.externalURL" target="_blank" rel="noopener noreferrer"><img :src="item.thumbnailURL" class="w-full block rounded" /></a>
+                                    <!-- <img :src="item.thumbnailURL" class="w-full block rounded hover:cursor-pointer" @click="playSound(item.previewUrl)" /> -->
                                 </div>
                                 <div class="p-5">
                                     <h3 class="text-white text-lg">{{ item.name }}</h3>
                                     <p class="text-white"><span v-for="artist in item.artists"> {{ artist }}, </span></p>
-                                    <p class="text-gray-400"><a :href="item.playlistUrl">{{ item.playlistName }}</a></p>
-                                    <p class="text-gray-400">Added: {{ formatDate(item.addedAt) }}</p>
                                     <p class="text-gray-400">Duration: {{ duration(item.duration) }}</p>
-                                    <p class="text-gray-400"><a :href="item.externalURL">Track URL</a></p>
+                                    <p class="text-gray-400">Added: {{ formatDate(item.addedAt) }}</p>
+                                    <p class="text-gray-400 hover:text-gray-100"><a :href="item.playlistUrl" target="_blank" rel="noopener noreferrer">{{ item.playlistName }}</a></p>
+
+                                    <p class="my-4 buttons w-full">
+                                        <a :href="item.externalURL" class="rounded-full block w-full text-black font-bold text-center bg-brand-green p-2" target="_blank" rel="noopener noreferrer">View Track</a>
+                                    </p>
 
                                     <!-- Play/Pause -->
-                                    <div v-if="item.previewUrl != 'null'" class="flex justify-center items-center m-5">
-                                        <button @click="playSound(item.previewUrl)" :show="!isAudioPlaying" class="w-24 h-24 rounded-full bg-gray-600 focus:outline-none"><i class="fa fa-play fa-2x text-white" id="play-btn">Play</i></button>
-                                        <button @click="stopSound(item.previewUrl)" :show="isAudioPlaying" class="w-24 h-24 rounded-full bg-gray-600 focus:outline-none"><i class="fa fa-play fa-2x text-white" id="play-btn">Pause</i></button>
-                                    </div>
+                                    <!-- <div v-if="item.previewUrl != 'null'" class="flex justify-center items-center m-5">
+                                        <button @click="playSound(item.previewUrl)" v-show="!isAudioPlaying" class="w-24 h-24 rounded-full bg-gray-600 focus:outline-none"><i class="fa fa-play fa-2x text-white" id="play-btn">Play</i></button>
+                                        <button @click="stopSound(item.previewUrl)" v-show="isAudioPlaying" class="w-24 h-24 rounded-full bg-gray-600 focus:outline-none"><i class="fa fa-play fa-2x text-white" id="play-btn">Pause</i></button>
+                                    </div> -->
 
                                 </div>
                             </div>
@@ -66,8 +77,8 @@
                     'Y9J24EDN41',
                     'f88fc1c54d13cf087ff21fac9294116f'
                 ),
+                isAudioPlaying: false
             };
-            isAudioPlaying: false
         },
         methods: {
             duration(millis) {
@@ -84,35 +95,33 @@
                 // Then specify how you want your dates to be formatted
                 return date.format('DD MMM YYYY');
             },
-            playSound(url) {
-                // Stop all asounds from playing
-                Howler.stop();
+            // playSound(url) {
+            //     // Stop all asounds from playing
+            //     Howler.stop();
 
-                // Setup and play the current sound
-                var sound = new Howl({
-                    src: [url],
-                    html5: true,
+            //     // Setup and play the current sound
+            //     var sound = new Howl({
+            //         src: [url],
+            //         html5: true,
 
-                    onplay: function() {
-                        this.isAudioPlaying = true
-                    },
-                    onpause: function() {
-                        this.isAudioPlaying = false
-                    },
-                    onstop: function() {
-                        this.isAudioPlaying = false
-                    },
-                    onend: function() {
-                        this.isAudioPlaying = false
-                    }
-                });
+            //         onplay: function() {
+            //             this.isAudioPlaying = true
+            //         },
+            //         onstop: function() {
+            //             this.isAudioPlaying = false
+            //         },
+            //         onend: function() {
+            //             this.isAudioPlaying = false
+            //         }
+            //     });
 
-                sound.play();
+            //     sound.play();
 
-            },
-            stopSound() {
-                Howler.stop();
-            }
+
+            // },
+            // stopSound() {
+            //     Howler.stop();
+            // }
         }
     };
 </script>
@@ -121,9 +130,14 @@
 @import './assets/base.css';
 
 #app {
-    /* max-width: 1280px; */
+    max-width: 1280px;
     margin: 0 auto;
     padding: 2rem;
     font-weight: normal;
 }
+
+.ais-SearchBox-input {
+    width: 100%;
+}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'brand-green': '#81b71a',
+        },
+      },
     },
     plugins: [
         require('@tailwindcss/forms'),
